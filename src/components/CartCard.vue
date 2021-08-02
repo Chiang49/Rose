bcd69c3690acdef2096c51e5badf4b8e6c827b26
Chiang <louis21029@gmail.com>
2021-08-02 18:54:46 +0800

    增加購物車表格

---
+++ src/components/CartCard.vue
@@ -0,0 +1,90 @@
+<template>
+  <li v-if="cartData.length === 0"
+      class="noProduct"
+  >
+    <p>購物車是空的喔，快去選購
+        <router-link class="d-inline" to="userProducts">商品</router-link>
+    吧</p>
+  </li>
+  <template v-else>
+    <li class="text-center mb-3" v-for="cartItem in cartData" :key="cartItem.id">
+        <div class="cartCard">
+        <div class="cartCard-inf">
+            <img
+            class="cartCard-image"
+            :src="cartItem.product.imageUrl"
+            :alt="cartItem.product.title"
+            />
+            <div class="cartCard-text">
+            <p class="cartCard-title">{{ cartItem.product.title }}</p>
+            <div class="input-group mb-2">
+                <input type="number" class="form-control"
+                    :value="cartItem.qty"
+                />
+                <span class="input-group-text">{{ cartItem.product.unit}}</span>
+            </div>
+            <p class="d-none d-md-block text-danger mb-2"
+                v-if="cartItem.product.category === '客制'"
+            >
+                此款為客制，下單後 2 個工作天會與您聯絡!
+            </p>
+            <p>總價：NT {{ cartItem.final_total }}</p>
+            </div>
+        </div>
+        <div class="cartCard-close">
+            <button type="button" class="btn-close" aria-label="Close"
+                    @click="deleteCart(cartItem.id)"
+            ></button>
+        </div>
+        </div>
+        <p class="d-md-none text-danger"
+        v-if="cartItem.product.category === '客制'"
+        >
+        此款為客制，下單後 2 個工作天會與您聯絡!
+        </p>
+    </li>
+  </template>
+</template>
+
+<script>
+export default {
+  props: {
+    cart: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      cartData: [],
+    };
+  },
+  watch: {
+    cart() {
+      this.cartData = this.cart;
+    //   console.log(this.cartData);
+    },
+  },
+  methods: {
+    // 刪除單一購物車
+    deleteCart(id) {
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart/${id}`;
+      this.$http.delete(api).then((res) => {
+        console.log(res);
+        if (res.data.success) {
+          this.$swal.fire({
+            icon: 'success',
+            title: res.data.message,
+          });
+          this.$emit('renderCart');
+        }
+      }).catch((err) => {
+        console.log(err);
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../assets/stylesheet/components/_cartCard.scss';
+</style>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,63 +1,83 @@
 <template>
   <Header></Header>
-  <div class="container">
-    <ul class="row">
-      <li>
-        <div class="cartCard">
-          <div class="cartCard-inf">
-            <img class="cartCard-image"
-                 src="https://images.unsplash.com/photo-1627626775846-122b778965ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
-                 alt="" />
-            <div class="cartCard-text">
-              <p class="cartCard-title">玫瑰花束</p>
-              <div class="input-group mb-2">
-                <input
-                  type="number"
-                  class="form-control"
-                />
-                <span class="input-group-text">束</span>
-              </div>
-              <p class="notice">
-                此款為客制，下單後 2 個工作天會與您聯絡!
-              </p>
-              <p>總價：NT 1000</p>
-            </div>
-          </div>
-          <div class="cartCard-close">
-            <button type="button" class="btn-close" aria-label="Close"></button>
-          </div>
-        </div>
-      </li>
+  <div class="container text-center">
+    <h2 class="subtitle">購物車</h2>
+    <ul class="cartTable">
+      <CartCard
+        :cart = "cartDatas"
+        @renderCart = "getCartsData"
+      ></CartCard>
     </ul>
+    <div class="row cartTotal">
+      <div class="col-md-6 cartTotal-btns">
+        <button
+          type="button"
+          class="btn btn-secondary me-2"
+        >我要結帳囉</button>
+        <button
+          type="button"
+          class="btn btn-danger"
+          @click="deleteCartAll"
+        >清空購物車</button>
+      </div>
+      <p class="col-md-6 cartTotal-price">總金額：NT {{ cartTotal.final_total }} 元</p>
+    </div>
   </div>
 </template>
 
 <script>
 import Header from '../components/Header.vue';
+import CartCard from '../components/CartCard.vue';
 
 export default {
   components: {
     Header,
+    CartCard,
   },
   data() {
     return {
+      cartTotal: {},
       cartDatas: [],
     };
   },
   methods: {
+    // 取得購物車資料
     getCartsData() {
       const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart`;
       this.$http
         .get(api)
         .then((res) => {
-          console.log(res);
+          // console.log(res);
           if (res.data.success) {
+            this.cartTotal = res.data.data;
             this.cartDatas = res.data.data.carts;
+            console.log(this.cartTotal);
           }
         })
         .catch((err) => {
           console.log(err);
         });
+    },
+    // 清空購物車
+    deleteCartAll() {
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/carts`;
+      this.$http.delete(api).then((res) => {
+        console.log(res);
+        if (res.data.success) {
+          this.$swal.fire({
+            icon: 'success',
+            title: res.data.message,
+          });
+          this.getCartsData();
+        } else {
+          this.$swal.fire({
+            icon: 'error',
+            title: res.data.message,
+          });
+        }
+      }).catch((err) => {
+        console.log(err);
+      });
     },
   },
   mounted() {
@@ -67,5 +87,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import "../assets/stylesheet/pages/_cart.scss";
+@import '../assets/stylesheet/pages/_cart.scss';
 </style>
