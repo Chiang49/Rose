4a2797940abaff961960afd8f6cfc185bc2adee9
Chiang <louis21029@gmail.com>
2021-09-03 19:11:06 +0800

    修改銷售排行榜樣式、隨機抓銷售排行榜前三名

--- src/components/HotProductCard.vue
+++ src/components/HotProductCard.vue
@@ -1,17 +1,12 @@
 <template>
-  <router-link class="hotProductCard" to="/product/-Mf7iysbKM9M9amWgnY9">
-    <div class="row align-items-center">
-      <div class="col-md-5 order-2 order-md-1">
-        <div>
-          <h3 class="hotProductCard-title">玫瑰盒</h3>
-          <p class="hotProductCard-price">NT {{ toCurrency(1250) }}</p>
-          <p class="hotProductCard-sell">目前已銷售 1000 盒</p>
-        </div>
-      </div>
-      <div class="col-md-7 p-md-4 p-lg-5 mb-3 mb-md-0 order-1 order-md-2">
-        <div class="squareRWD">
-          <img src="https://images.unsplash.com/photo-1613414312279-0549888b3ab1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="">
-        </div>
+  <router-link class="hotProductCard" :to="`/product/${hotProduct.id}`">
+    <img :src="`${hotProduct.imageUrl}`" :alt="`${hotProduct.title}`">
+    <div class="hotProductCard-text">
+      <h3 class="hotProductCard-rank">TOP {{rank + 1}}</h3>
+      <div class="hotProductCard-content">
+        <h3 class="hotProductCard-title">{{ hotProduct.title }}</h3>
+        <p class="hotProductCard-price">NT {{ toCurrency(hotProduct.price) }}</p>
+        <p class="hotProductCard-sell">目前已銷售 1000 盒</p>
       </div>
     </div>
   </router-link>
@@ -19,6 +14,16 @@
 
 <script>
 export default {
-
+  props: {
+    hotProduct: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    rank: {
+      type: Number,
+    },
+  },
 };
 </script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,10 +1,15 @@
 <template>
+  <Loading :loading="is_loading" />
   <IndexHeader></IndexHeader>
-  <!-- 介紹文 -->
+  <!-- 排行榜 -->
   <div class="container mb-10">
     <Divider :title="dividerTitle.section1"></Divider>
     <section class="hotProduct">
-      <HotProductCard></HotProductCard>
+      <div class="row">
+        <div class="col-md-4" v-for="(item, key) in rankTop" :key="key">
+          <HotProductCard :hotProduct="item" :rank="key"></HotProductCard>
+        </div>
+      </div>
     </section>
   </div>
   <!-- 活動區 -->
@@ -45,7 +50,48 @@ export default {
         section1: 'Hot',
         section2: 'Product',
       },
+      is_loading: false,
+      allProducts: [],
+      rankTop: [],
     };
+  },
+  methods: {
+    // 取得所有商品資料
+    getAllProducts() {
+      this.is_loading = true;
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products/all`;
+      this.$http.get(api).then((res) => {
+        if (res.data.success) {
+          this.allProducts = res.data.products;
+          this.getRankTop();
+          this.is_loading = false;
+        } else {
+          this.$swal.fire({
+            icon: 'error',
+            title: '資料讀取失敗，請重新載入',
+          });
+          this.is_loading = true;
+        }
+      }).catch((err) => {
+        console.log(err);
+      });
+    },
+    // 隨機篩選前 3 名
+    getRankTop() {
+      for (let i = 0; this.rankTop.length < 3; i += 1) {
+        const rnd = Math.floor(Math.random() * this.allProducts.length);
+        this.rankTop.forEach((item, key) => {
+          if (item.id === this.allProducts[rnd].id) {
+            this.rankTop.splice(key, 1);
+            i -= 1;
+          }
+        });
+        this.rankTop.push(this.allProducts[rnd]);
+      }
+    },
+  },
+  created() {
+    this.getAllProducts();
   },
 };
 </script>
